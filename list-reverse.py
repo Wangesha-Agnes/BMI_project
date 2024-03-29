7b6b638674df1c72d1e76adfebf8e6e29f21b8aa
Nancy-wainaina <wainainanancywambui@gmail.com>
2024-03-29 10:29:58 +0300

    created file

---
+++ list-reverse.py
@@ -0,0 +1,17 @@
+#list reverse
+fruits=['apple','cherry' ,'mango','banana','berry']
+fruits.reverse()
+print(fruits)
+
+
+names=['Agnes','Redempta','Wangesha','Ngina','Mary']
+names.reverse()
+print(names)
+
+
+#list reverse
+
+
+list1 = ["Agnes", 22, True, "female"]
+list1.reverse()
+print(list1)
